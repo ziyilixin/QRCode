3c84ab37a801a4ccc173e74d5c4473534a78ebc7
coderCF <1559878532@qq.com>
2018-03-16 10:35:11 +0800

    Initial Commit

---
+++ QRCode/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QRCode
+//
+//  Created by WCF on 2018/3/16.
+//  Copyright © 2018年 BCQ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ QRCode/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  QRCode
+//
+//  Created by WCF on 2018/3/16.
+//  Copyright © 2018年 BCQ. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
